888c543feb2684f4cbbcdc13cf6d916d87a6ff42
cjsqldbtn <cjsqldbtn@gmail.com>
2026-07-07 18:01:13 +0900

    modify: placeSQL

--- src/main/java/com/nh/dao/PlaceDaoImpl.java
+++ src/main/java/com/nh/dao/PlaceDaoImpl.java
@@ -1,6 +1,8 @@
 package com.nh.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,4 +24,99 @@ public class PlaceDaoImpl implements PlaceDao {
 	public List<String> getPlaceImages(String placeId) {
 		return sqlSession.selectList("placeMapper.selectPlaceImages", placeId);
 	}
+	
+	// 장소 정보 조회
+	@Override
+	public Map<String, Object> viewPlaceDetails(String placeId, int memberId) {
+		Map<String, Object> map1 = new HashMap<>();
+		map1.put("placeId", placeId);
+		map1.put("memberId", memberId);
+		
+		return sqlSession.selectOne("placeMapper.selectPlaceDetails", map1);
+	}
+	
+	// 찜한 장소 삽입
+	@Override
+	public void addLikedPlace(int memberId, String placeId) {
+		Map<String, Object> map1 = new HashMap<>();
+		map1.put("placeId", placeId);
+		map1.put("memberId", memberId);
+		
+		sqlSession.insert("placeMapper.insertLikedPlace", map1);
+	}
+	
+	// 찜한 장소 삭제
+	@Override
+	public void deleteLikedPlace(int memberId, String placeId) {
+		Map<String, Object> map1 = new HashMap<>();
+		map1.put("placeId", placeId);
+		map1.put("memberId", memberId);
+		
+		sqlSession.delete("placeMapper.deleteLikedPlace", map1);
+	}
+	
+	// 장소 댓글 목록 조회
+	@Override
+	public List<Map<String, Object>> getReviews(String placeId, int start, int end) {
+		Map<String, Object> map1 = new HashMap<>();
+		map1.put("placeId", placeId);
+		map1.put("start", start);
+		map1.put("end", end);
+		return sqlSession.selectList("placeMapper.selectReviews", map1);
+	}
+	
+	// 장소 댓글 삽입
+	@Override
+	public void addReview(int memberId, String placeId, String content, int rating, String image) {
+		Map<String, Object> map1 = new HashMap<>();
+		map1.put("placeId", placeId);
+		map1.put("memberId", memberId);
+		map1.put("content", content);
+		map1.put("rating", rating);
+		map1.put("image", image==null ? "" : image);
+		
+		sqlSession.insert("placeMapper.insertReview", map1);
+	}
+	
+	// 장소 댓글 수정
+	@Override
+	public void modifyReview(int reviewIdx, int memberId, String content, int rating, String image) {
+		Map<String, Object> map1 = new HashMap<>();
+		map1.put("reviewIdx", reviewIdx);
+		map1.put("memberId", memberId);
+		map1.put("content", content);
+		map1.put("rating", rating);
+		map1.put("image", image==null ? "" : image);
+		
+		sqlSession.update("placeMapper.updateReview", map1);
+	}
+	
+	// 장소 댓글 삭제
+	@Override
+	public void deleteReview(int reviewIdx, int memberId) {
+		Map<String, Object> map1 = new HashMap<>();
+		map1.put("reviewIdx", reviewIdx);
+		map1.put("memberId", memberId);
+
+		sqlSession.delete("placeMapper.deleteReview", map1);
+	}
+	
+	// 장소 삽입
+	@Override
+	public void addPlace(String placeId, String name, String category, String address, Number lat, Number lng,
+			String websiteUrl, String businessHours) {
+		Map<String, Object> map1 = new HashMap<>();
+		map1.put("placeId", placeId);
+		map1.put("name", name);
+		map1.put("category", category);
+		map1.put("address", address);
+		map1.put("lat", lat);
+		map1.put("lng", lng);
+		map1.put("websiteUrl", websiteUrl==null ? "" : websiteUrl);
+		map1.put("businessHours", businessHours==null ? "" : businessHours);
+		
+		sqlSession.insert("placeMapper.insertPlace", name);
+	}
+	
+	
 }
--- src/test/java/com/nh/TravelPlanner/PlaceDaoTest.java
+++ src/test/java/com/nh/TravelPlanner/PlaceDaoTest.java
@@ -3,12 +3,14 @@ package com.nh.TravelPlanner;
 import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.nh.dao.PlaceDao;
 
@@ -48,5 +50,135 @@ public class PlaceDaoTest {
 		assertNotNull("리스트는 널이 아님", listimages);
 		
 		System.out.println(listimages);
+	}
+	
+	// 장소 정보 조회 테스트
+	@Test
+	public void testViewPlaceDetails() {
+		// 1) Given
+		String placeId = "ChIJofoWUQCNaDURDqIZjAjYMU8";
+		int memberId = 1;
+		
+		// 2) When
+		Map<String, Object> listDetail = pDao.viewPlaceDetails(placeId, memberId);
+		
+		// 3) Then
+		assertNotNull("리스트는 널이 아님", listDetail);
+		
+		System.out.println(listDetail);
+	}
+	
+	// 찜한 장소 삽입 테스트
+	@Test
+	@Transactional
+	public void testAddLikedPlace() {
+		// 1) Given
+		String placeId = "ChIJofoWUQCNaDURDqIZjAjYMU8";
+		int memberId = 3;
+		
+		// 2) When
+		pDao.addLikedPlace(memberId, placeId);
+		
+		// 3) Then : sql 오류가 없다면 성공
+	}
+	
+	// 찜한 장소 삭제 테스트
+	@Test
+	@Transactional
+	public void testDeleteLikedPlace() {
+		// 1) Given
+		String placeId = "ChIJofoWUQCNaDURDqIZjAjYMU8";
+		int memberId = 1;
+		
+		// 2) When
+		pDao.deleteLikedPlace(memberId, placeId);
+		
+		// 3) Then : sql 오류가 없다면 성공
+	}
+	
+	// 장소 댓글 목록 조회 테스트
+	@Test
+	public void testViewReviews() {
+		// 1) Given
+		String placeId = "ChIJofoWUQCNaDURDqIZjAjYMU8";
+		int start = 1;
+		int end = 3;
+		
+		// 2) When
+		List<Map<String, Object>> listReview = pDao.getReviews(placeId, start, end);
+		
+		// 3) Then
+		assertNotNull("리스트는 널이 아님", listReview);
+		
+		System.out.println(listReview);
+	}
+	
+	// 장소 댓글 삽입 테스트
+	@Test
+	@Transactional
+	public void testAddReview() {
+		// 1) Given
+		String placeId = "ChIJofoWUQCNaDURDqIZjAjYMU8";
+		int memberId = 1;
+		String content = "테스트 내용";
+		int rating = 3;
+		String image = null;
+		
+		// 2) When
+		pDao.addReview(memberId, placeId, content, rating, image);
+		
+		// 3) Then : sql 오류가 없다면 성공
+	}
+	
+	// 장소 댓글 수정 테스트
+	@Test
+	@Transactional
+	public void testModifyReview() {
+		// 1) Given
+		int reviewIdx = 1;
+		int memberId = 1;
+		String content = "테스트 수정 내용";
+		int rating = 3;
+		String image = "수정";
+		
+		// 2) When
+		pDao.modifyReview(reviewIdx, memberId, content, rating, image);
+		
+		// 3) Then : sql 오류가 없다면 성공
+	}
+	
+	// 장소 댓글 삭제 테스트
+	@Test
+	@Transactional
+	public void testDeleteReview() {
+		// 1) Given
+		int reviewIdx = 1;
+		int memberId = 1;
+		
+		// 2) When
+		pDao.deleteReview(reviewIdx, memberId);
+		
+		// 3) Then : sql 오류가 없다면 성공
+	}
+	
+	// 장소 삽입 테스트
+	// 타입 에러
+	@Test
+	@Transactional
+	public void testAddPlace() {
+		// 1) Given
+		String placeId = "테스트 아이디";
+		String name = "테스트 장소 이름";
+		String category = "ex";
+		String address = "테스트 주소";
+		Number lat = 10.001;
+		Number lng = 10.001;
+		String websiteUrl = "테스트 링크";
+		String businessHours = "테스트 영업시간";
+		
+		// 2) When
+		pDao.addPlace(placeId, name, category, address, lat, lng, websiteUrl, businessHours);
+		
+		// 3) Then : sql 오류가 없다면 성공
 	}
 }
