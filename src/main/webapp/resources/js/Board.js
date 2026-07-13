56c2672cb33818f95e8c18e4e15abc08bbdaca0a
cjsqldbtn <cjsqldbtn@gmail.com>
2026-07-13 17:56:44 +0900

    수정: 게시글 댓글 불러오기, 댓글 수정

--- src/main/webapp/resources/js/Board.js
+++ src/main/webapp/resources/js/Board.js
@@ -21,10 +21,11 @@ function newPage(pageNum, bno) {
 		return response.json();
 	})
 	.then(function(data) {
-		console.log(data);
 		$("#topPageNation>div>span:nth-child(1)").html(gPageNum);
 		$("#topPageNation>div>span:nth-child(2)").html(data.lastPageNum);
+		$("#commentList>*").remove();
 		let arr = data.comments;
+		let writerId = $("#commentList").attr('data-writerId');
 		for(let i=0;i<arr.length;i++){
 			let item = arr[i];
 			const str1 = `
@@ -41,27 +42,51 @@ function newPage(pageNum, bno) {
 			const str3 = `
 							<div>${item.nickName}</div>
 						</div>
-						<div>
-							<div class="moveDate" data-memberId="${item.memberId}">
-								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-									<path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-								</svg>
-								수정
-							</div>
-							<div class="moveDate" data-memberId="${item.memberId}">
-								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-									<path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-								</svg>
-								삭제
+			`;
+			let str4 = "";
+			if(item.memberId==writerId) {
+				str4 = `
+							<div>
+								<div class="moveDate">
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+										<path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+									</svg>
+									수정
+								</div>
+								<div class="moveDate" data-cno="${item.cno}">
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+										<path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+									</svg>
+									삭제
+								</div>
 							</div>
 						</div>
+						<input data-cno="${item.cno}" id="modifyComment" type="text" value="${item.content}"/>
+				`;
+			}
+			else {
+				str4 = `
 					</div>
-					<div>${item.content}</div>
+					<div>${item.content}</div>				
+				`
+			}
+			const str5 = `
 					<div>${item.finalDate}</div>
 				</div>
 			`;
-			$("#commentList").append(str1+str2+str3);
+			$("#commentList").append(str1+str2+str3+str4+str5);
 		}
+		$("#bottomPageNation>*").remove();
+		if(parseInt((gPageNum-1)/5)*5+1>5) $("#bottomPageNation").append(`<div id="beforePage" class="moveDate">이전</div>`);
+		for(let i=parseInt((gPageNum-1)/5)*5+1;i<=parseInt((gPageNum-1)/5)*5+5;i++){
+			if(i>data.lastPageNum) return;
+			if(i==gPageNum) {
+				$("#bottomPageNation").append(`<div class="fixDate">${i}</div>`);
+			} else {
+				$("#bottomPageNation").append(`<div class="moveDate nthPage">${i}</div>`);
+			}
+		}
+		if(parseInt((gPageNum-1)/5)*5+5<data.lastPageNum+1) $("#bottomPageNation").append(`<div id="nextPage" class="moveDate">다음</div>`);
 	})
 	.catch(function(error) {
 		alert("에러! : " + error);
@@ -141,41 +166,96 @@ $(function() {
 	});
 	// *************** 예산 아래 버튼 ****************
 	// 게시글 복제
-	$(".other>div").on('click', function () {
+	$(document).on("click", ".other>div", function() {
 		alert("게시글이 복제되었습니다!");
 		location.href="/TravelPlanner/newplan/start";
 	});
 	// 삭제
-	$(".my>div:nth-child(1)").on('click', function () {
+	$(document).on("click", ".my>div:nth-child(1)", function() {
 		alert("정말 삭제하시겠습니까?");
 		location.href="/TravelPlanner";
 	});
 	// 수정
-	$(".my>div:nth-child(2)").on('click', function () {
+	$(document).on("click", ".my>div:nth-child(2)", function() {
 		location.href = $(location).attr('pathname') + "/week";
 	});
 	// AI 비용
-	$(".my>div:nth-child(3)").on('click', function () {
+	$(document).on("click", ".my>div:nth-child(3)", function() {
 		alert("계산해드립니당");
 	});
 	// 캘린더 동기화
-	$(".my>div:nth-child(4)").on('click', function () {
+	$(document).on("click", ".my>div:nth-child(4)", function() {
 		alert("동기화!");
 	});
+	
 	// *******************댓글*********************
 	// 댓글 페이지네이션
-	$("#beforePage").on('click', function () {
-		// 페이지 넘기는 div 수정
+	$(document).on("click", "#beforePage", function() {
+		gPageNum = parseInt((gPageNum-1)/5)*5;
+		newPage(gPageNum,bno);
 	});
-	$("#nextPage").on('click', function () {
-		// 페이지 넘기는 div 수정
+	$(document).on("click", "#nextPage", function() {
+		gPageNum = parseInt((gPageNum-1)/5)*5+6;
+		newPage(gPageNum,bno);
 	});
-	$(".nthPage").on('click', function () {
+	$(document).on("click", ".nthPage", function() {
 		let page = Number($(this).html());
-		if(page!=gPageNum) {
-			newPage(page);
-			gPageNum = page;
-		}
+		gPageNum = page;
+		newPage(gPageNum,bno);
+	});
+	
+	// 포커스 발생 시 현재 댓글 저장
+	$('.comment > input').on('focus', function () {
+	    $(this).data('original-value', $(this).val());
+	});
+	// 엔터키로도 저장 작동
+	$('.comment > input').on('keydown', function (e) {
+	    if(e.keyCode == 13){
+	        $(this).blur();
+	    }
+	});
+	// 수정 버튼 클릭 시 포커스
+	$(document).on("click", ".comment > div:nth-child(1) > div:nth-child(2) > div", function() {
+	    $(this).parent().parent().parent().find("input").focus();
+	});
+	// 댓글 수정
+	$(document).on("blur", ".comment > input", function() {
+		const originalContent = String($(this).data('original-value'));
+    	const currentContent = $(this).val();
+    	const cno = $(this).data('cno');
+    	
+		// 제목에 변경 사항이 없다면 종료
+		if (originalContent === currentContent) return;
+		
+		const jsonData = {
+			"content" : currentContent,
+			"cno" : cno,
+		};
+		const initData = {
+			method: "post",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(jsonData)
+		};
+		fetch("/TravelPlanner/updateComment", initData)
+		.then(function(response) {
+			return response.text();
+		})
+		.then(function(data) {
+			console.log(data);
+		})
+		.catch(function(error) {
+			alert("에러! : 댓글 저장에 문제가 발생했습니다. 다시 시도해주세요." + error);
+		});
+	});
+	// 댓글 삭제
+	$(document).on("click", ".comment>div>div:nth-child(2)>div:nth-child(2)", function() {
+		alert("정말 삭제 하시나요?");
+	});
+	// 댓글 등록
+	$(document).on("click", "#commentInput>div", function() {
+		alert("등록~");
 	});
 	// 찜 색칠
 	$("#content3 > div:nth-child(1) > div > div:nth-child(1)").on('click', function () {
@@ -210,17 +290,5 @@ $(function() {
 		}
 		
 		$(this).find("svg").toggleClass("fillHeart");
-	});
-	// 댓글 수정
-	$(".comment>div>div:nth-child(2)>div:nth-child(1)").on('click', function () {
-		alert("수정 버튼!");
-	});
-	// 댓글 삭제
-	$(".comment>div>div:nth-child(2)>div:nth-child(2)").on('click', function () {
-		alert("정말 삭제 하시나요?");
-	});
-	// 댓글 등록
-	$("#commentInput>div").on('click', function () {
-		alert("등록~");
 	});
 });
