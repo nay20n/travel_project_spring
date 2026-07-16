52a7399eb9acdeb3b41d86c8c70ae6ebbabd0940
nayeon <nayeon@localhost>
2026-07-16 17:39:58 +0900

    Data: date range picker API

--- src/main/webapp/resources/js/Data.js
+++ src/main/webapp/resources/js/Data.js
@@ -1,27 +1,60 @@
 function getUrlParams() {     
-	    var params = {};  
-	    
-	    window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, 
-	    	function(str, key, value) { 
-	        	params[key] = value; 
-	        }
-	    );     
-	    
-	    return params; 
+    var params = {};  
+    
+    window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, 
+    	function(str, key, value) { 
+        	params[key] = value; 
+        }
+    );     
+    
+    return params; 
 }
-
+function clickInput() {
+	$("#selectDate > input").click();
+}
 let isActive = false;
+let startDate;
+let endDate;
 
 $(function(){
+
 	// <-이미지 클릭하면 전 페이지로
 	$("#header > img").click(function() {
 		history.back();
 	});
 	
-	$("#selectDate > img").click(function() {
-		isActive = true;
-		$("#nextBtn > div").addClass("coloredBtn");
+	// 데이터 피커 인풋
+	$('#selectDate > input').daterangepicker({
+		locale: {
+			"separator": " ~ ",                     
+			"format": 'YY년 MM월 DD일',     		
+			"yearSuffix": "년",
+			"daysOfWeek": ["일", "월", "화", "수", "목", "금", "토"],
+			"monthNames": ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+			"showMonthAfterYear": true //년도 먼저 나오고, 뒤에 월 표시
+		},
+		"autoApply": true, 
+		"showDropdowns": true, // 월/년 셀렉터 
+		"minDate": new Date(),
+		"maxYear": new Date().getFullYear() + 5,
+		"inline": true,
+		"linkedCalendars": false,
+		autoUpdateInput: false
+		
+		}, function(start, end, label) {
+			$('#selectDate > input').val(start.format('YY년 MM월 DD일') + ' ~ ' + end.format('YY년 MM월 DD일'));
+			startDate = start.format('YY.MM.DD');
+			endDate = end.format('YY.MM.DD');
+			
+			isActive = true;
+			$("#nextBtn > div").addClass("coloredBtn");
+		    //console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
 	});
+	// 어플라이 버튼을 클릭 시 인풋 클릭한걸로 치기 
+	$('#selectDate > input').on('apply.daterangepicker', function(ev, picker) {
+		$("#selectDate > input").trigger("click");
+	});
+	
 	
 	// 다음 페이지 클릭하면 도착지 페이지로 이동
 	$("#nextBtn > div").click(function(){
@@ -33,9 +66,6 @@ $(function(){
 		let arrCity = "부산광역시";
 		// 출발 아이디 가져오기 
 		let startId = getUrlParams().startId;
-		// 여행 기간 설정 
-		let endDate = "2026-09-02";	
-		let startDate = "2026-09-01";
 		
 		const jsonData = {
 			arrId : arrId,
@@ -56,11 +86,10 @@ $(function(){
 			return response.json();
 		})
 		.then(function(data){
-			console.log(data);
-			
+			//console.log(data);
 			let bno = data;
-			// alert(bno);
-			location.href="../plan/" + bno;
+			if(isActive)
+				location.href="../plan/" + bno;
 			
 		})
 		.catch(function(error){
@@ -69,4 +98,8 @@ $(function(){
 		
 	});
 	
+	
+	
 });
+
+
