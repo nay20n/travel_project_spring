0eea96dfd286ffbf648b467c49a9e61fb68cc2d3
HyunAh <cjsqldbtn@gmail.com>
2026-07-23 17:55:14 +0900

    이름수정

--- src/main/java/com/nh/controller/TravelRestController.java
+++ src/main/java/com/nh/controller/TravelRestController.java
@@ -22,7 +22,7 @@ import com.nh.service.MemberService;
 import com.nh.service.PlaceService;
 
 @RestController
-public class RestContoller {
+public class TravelRestController {
 	@Autowired
 	CommentService cSvc;
 	@Autowired
