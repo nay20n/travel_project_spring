622a7cea07ea2dfe5b0e903e213fba82e283fe12
nay20n <kny1791@gmail.com>
2026-07-28 16:17:58 +0900

    bx슬라이더 적용

--- src/main/webapp/resources/js/ResetPw.js
+++ src/main/webapp/resources/js/ResetPw.js
@@ -4,14 +4,29 @@ $(function (){
 		
 		let email = $("#main > input").val();
 		
-		if(email=="a"){  //이메일을 잘 입력했을 떄
-			$("#main > div:nth-child(7)").removeClass("show");
-			$("#main > input").removeClass("borderWraning");
-			alert("비밀번호 재설정 메일을 보냈습니다.");
-		}
-		else{ //이메일을 잘못 압력 못햇을 떄
-			$("#main > div:nth-child(7)").addClass("show");
-			$("#main > input").addClass("borderWraning");
-		}
+		fetch("getEmail", {method:'post'})
+		.then(function(response){
+			return response.text();
+		})
+		.then(function(data){
+			//console.log(data);
+			if(email===data){  //이메일을 잘 입력했을 떄
+				$("#main > div:nth-child(7)").removeClass("show");
+				$("#main > input").removeClass("borderWraning");
+				
+				
+				
+				
+				alert("비밀번호 재설정 메일을 보냈습니다.");
+			}
+			else{ //이메일을 잘못 입력 못햇을 떄
+				$("#main > div:nth-child(7)").addClass("show");
+				$("#main > input").addClass("borderWraning");
+			}
+		})
+		.catch(function(error){
+			alert("에러! : " + error);
+		})
+		
 	});
 });
